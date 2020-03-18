69a0299af9c8308a6c02cddc76ef0fac55690aec
Sean Ives <me@seanives.com>
2020-03-18 16:17:09 +0000

    test(parser): refine tests for elements selected on products page

---
+++ src/test/java/com/seanives/productscraper/parser/ProductParserFixtures.java
@@ -0,0 +1,8 @@
+package com.seanives.productscraper.parser;
+
+public class ProductParserFixtures {
+    public static final String TEST_PRODUCT_TITLE = "test product title";
+    public static final String TEST_PRODUCT_DESCRIPTION = "test product description";
+    public static final double TEST_PRODUCT_PRICE = 123.45d;
+    public static final int TEST_PRODUCT_KCALS = 123;
+}
--- src/test/java/com/seanives/productscraper/parser/ProductParserTest.java
+++ src/test/java/com/seanives/productscraper/parser/ProductParserTest.java
@@ -13,14 +13,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static com.seanives.productscraper.parser.ProductParserFixtures.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-
 public class ProductParserTest {
   static final String testProductUrl = "http://example.com/product-page.html";
+  static final String testProductDetailsUrl = "http://example.com/product-details-page.html";
+
+  static final ProductModel testProductModel =
+      new ProductModel(
+          TEST_PRODUCT_TITLE,
+          Optional.of(TEST_PRODUCT_KCALS),
+          TEST_PRODUCT_PRICE,
+          TEST_PRODUCT_DESCRIPTION);
 
   @Mock Presenter mockPresenter;
   @Mock Document mockDocument;
@@ -38,12 +47,13 @@ public class ProductParserTest {
   @DisplayName("should get a list of parsed products")
   void getProducts() {
     doReturn(mockDocument).when(productParser).getDocument(anyString());
-    List mockProductsList = mock(List.class);
-    doReturn(mockProductsList).when(productParser).parseProductsPage(any());
+    doReturn(Arrays.asList(testProductModel)).when(productParser).parseProductsPage(any());
     productParser.getProducts(mockPresenter);
-    verify(productParser, times(1)).getDocument(testProductUrl);
-    verify(productParser, times(1)).parseProductsPage(mockDocument);
-    verify(mockPresenter, times(1)).parsingCompletedSuccesfully(mockProductsList);
+    ArgumentCaptor<List<ProductModel>> productListCaptor = ArgumentCaptor.forClass(List.class);
+    verify(mockPresenter, times(1)).parsingCompletedSuccesfully(productListCaptor.capture());
+    assertThat(productListCaptor.getValue(), is(notNullValue()));
+    assertThat(productListCaptor.getValue().size(), is(1));
+    assertThat(productListCaptor.getValue().get(0), is(equalTo(testProductModel)));
   }
 
   @Test
@@ -55,41 +65,66 @@ public class ProductParserTest {
     assertThat(productList, is(notNullValue()));
     assertThat(productList.size(), is(equalTo(1)));
     verify(productParser, times(1)).parseProduct(mockProduct);
+    verify(mockProductsPage, times(1)).select(".productLister .product");
   }
 
   @Test
   @DisplayName("should parse a product")
   void parseProduct() {
+    Element productPage = mock(Element.class);
+    Element productNameAndPromos = mock(Element.class);
+    Elements products = mock(Elements.class);
+    doReturn(productNameAndPromos).when(products).first();
+    Element pricingDetails = mock(Element.class);
+    Elements pricing = mock(Elements.class);
+    doReturn(pricingDetails).when(pricing).first();
+    doReturn(products, pricing).when(productPage).select(anyString());
+    doReturn(mockDocument).when(productParser).getDocument(anyString());
     ProductModel productModel = productParser.parseProduct(mockProduct);
     assertThat(productModel, is(notNullValue()));
+    verify(mockProduct, times(1)).select(".productInfo .productNameAndPromotions a");
+    verify(mockProduct, times(1)).select(".pricePerUnit");
+    verify(productParser, times(1)).getDocument(testProductDetailsUrl);
+    verify(productParser, times(1)).getTitle(mockProduct);
+    verify(productParser, times(1)).getPricePerUnit(mockProduct);
+    verify(productParser, times(1)).getDescription(mockDocument);
+    verify(productParser, times(1)).getKCals(mockDocument);
+
   }
 
   @Test
   @DisplayName("should return the product title")
   void getTitle() {
-    String title = productParser.getTitle(mockProduct);
-    assertThat(title, is(notNullValue()));
+    Element productNameAndPromos = mock(Element.class);
+    String title = productParser.getTitle(productNameAndPromos);
+    assertThat(title, is(equalTo(TEST_PRODUCT_TITLE)));
+    verify(productNameAndPromos, times(1)).text();
   }
 
   @Test
   @DisplayName("should return the price per unit")
   void getPricePerUnit() {
-    double price = productParser.getPricePerUnit(mockProduct);
-    assertThat(price, is(greaterThan(0.0d)));
+    Element pricingDetails = mock(Element.class);
+    double price = productParser.getPricePerUnit(pricingDetails);
+    assertThat(price, is(equalTo(TEST_PRODUCT_PRICE)));
+    verify(pricingDetails, times(1)).getAllElements();
   }
 
   @Test
   @DisplayName("should return the description")
   void getDescription() {
     String description = productParser.getDescription(mockProduct);
-    assertThat(description, is(notNullValue()));
+    assertThat(description, is(equalTo(TEST_PRODUCT_DESCRIPTION)));
+    verify(mockProduct, times(1)).select(".mainProductInfo #information.section h3 + *");
   }
 
   @Test
   @DisplayName("should return kcals per 100g")
   void getKCals() {
     Optional<Integer> kcals = productParser.getKCals(mockProduct);
-    assertThat(kcals, is(notNullValue()));
+    assertTrue(kcals.isPresent());
+    assertThat(kcals.get(), is(equalTo(TEST_PRODUCT_KCALS)));
+    verify(mockProduct, times(1)).select(".mainProductInfo #information.section h3 + *");
   }
 
   @Test
