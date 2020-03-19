d5de6fca33716c3e980a75e96ab160c4345e1912
Sean Ives <me@seanives.com>
2020-03-19 17:07:27 +0000

    refactor(reporter): tests passing

--- src/main/java/com/seanives/productscraper/reporter/ProductReporter.java
+++ src/main/java/com/seanives/productscraper/reporter/ProductReporter.java
@@ -2,15 +2,20 @@ package com.seanives.productscraper.reporter;
 
 import com.seanives.productscraper.model.ProductResultsModel;
 import com.seanives.productscraper.reporter.report.BasicJsonProductReport;
+import com.seanives.productscraper.reporter.report.JsonReportRenderer;
+import com.seanives.productscraper.reporter.report.Report;
 
 public class ProductReporter {
   private final ProductResultsModel productResultsModel;
+  private final Report report;
 
   public ProductReporter(ProductResultsModel productResultsModel) {
     this.productResultsModel = productResultsModel;
+    this.report = new BasicJsonProductReport(productResultsModel, new JsonReportRenderer());
   }
 
-  public BasicJsonProductReport generateReport() {
-    return null;
+  public Report generateReport() {
+    report.render();
+    return report;
   }
 }
