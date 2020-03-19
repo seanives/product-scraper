d26db93e3c0bb605543f0faa06a39e3b84e1f0c3
Sean Ives <me@seanives.com>
2020-03-19 08:09:30 +0000

    refactor(parser): tests passing

--- src/main/java/com/seanives/productscraper/parser/ProductParser.java
+++ src/main/java/com/seanives/productscraper/parser/ProductParser.java
@@ -4,10 +4,15 @@ import com.seanives.productscraper.Presenter;
 import com.seanives.productscraper.model.ProductModel;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ProductParser {
   final String productsPageUrl;
@@ -18,43 +23,102 @@ public class ProductParser {
     this.productsPageUrl = productsPageUrl;
   }
 
-  public void getProducts(final Presenter presenter) {
+  public void getProducts(final Presenter presenter) throws IOException {
     Document productsPage = getDocument(productsPageUrl);
     List<ProductModel> productsList = parseProductsPage(productsPage);
 
     presenter.parsingCompletedSuccesfully(productsList);
   }
 
-  List<ProductModel> parseProductsPage(final Element productsPage) {
+  List<ProductModel> parseProductsPage(final Element productsPage) throws IOException {
+    Elements products = productsPage.select(".productLister .product");
     List<ProductModel> productList = new ArrayList<>();
-    for (Element product : productsPage.getAllElements()) {
+    for (Element product : products) {
       productList.add(parseProduct(product));
     }
     return productList;
   }
 
-  ProductModel parseProduct(final Element product) {
-    return new ProductModel(
-        getTitle(product), getKCals(product), getPricePerUnit(product), getDescription(product));
+  ProductModel parseProduct(final Element product) throws IOException {
+    Element productNameAndPromos =
+        Optional.ofNullable(product.select(".productInfo .productNameAndPromotions a").first())
+            .get();
+    String productTitle = getTitle(productNameAndPromos);
+
+    String productDetailsPageUrl = productNameAndPromos.absUrl("href");
+    Document productDetails = getDocument(productDetailsPageUrl);
+    String description = getDescription(productDetails);
+    Optional<Integer> kCals = getKCals(productDetails);
+
+    Element pricingDetails = Optional.of(product.select(".pricePerUnit").first()).get();
+    double pricePerUnit = getPricePerUnit(pricingDetails);
+
+    return new ProductModel(productTitle, kCals, pricePerUnit, description);
   }
 
-  String getTitle(final Element product) {
-    return null;
+  String getTitle(final Element productNameAndPromos) {
+    return productNameAndPromos.text();
   }
 
-  String getDescription(final Element product) {
-    return null;
+  String getDescription(final Element productDetails) {
+    return parseChildElements(
+            productDetails,
+            ".mainProductInfo #information.section h3 + *",
+            element ->
+                element.getElementsByTag("p").stream()
+                    .map(Element::text)
+                    .filter(content -> !"".equals(content.trim()))
+                    .findFirst()
+                    .get(),
+            productTextElement ->
+                Optional.ofNullable(productTextElement.previousElementSibling())
+                    .map(prev -> prev.text().equals("Description"))
+                    .orElse(false))
+        .stream()
+        .findFirst()
+        .get();
   }
 
-  Optional<Integer> getKCals(final Element product) {
-    return null;
+  Optional<Integer> getKCals(final Element productDetails) {
+    return parseChildElements(
+            productDetails,
+            ".mainProductInfo #information.section h3 + *",
+            element ->
+                element.getElementsByTag("td").stream()
+                    .map(Element::text)
+                    .filter(content -> content.endsWith("kcal"))
+                    .map(
+                        kcalText ->
+                            Integer.parseUnsignedInt(kcalText.replaceAll("([0-9]+).*", "$1")))
+                    .findFirst(),
+            productTextElement ->
+                Optional.ofNullable(productTextElement.previousElementSibling())
+                    .map(prev -> prev.text().equals("Nutrition"))
+                    .orElse(false))
+        .stream()
+        .findFirst()
+        .orElse(Optional.empty());
   }
 
-  double getPricePerUnit(final Element product) {
-    return 0.0d;
+  double getPricePerUnit(final Element pricingDetails) {
+    return pricingDetails.getAllElements().stream()
+        .map(Element::text)
+        .findFirst()
+        .map(priceText -> priceText.replaceAll("£(0|(?:[1-9][0-9]*))([.][0-9]+)?.*", "$1$2"))
+        .map(Double::parseDouble)
+        .get();
   }
 
-  public Document getDocument(final String pageUrl) {
-    return null;
+  <T> List<T> parseChildElements(
+      final Element doc,
+      final String selector,
+      final Function<Element, T> elementMapper,
+      final Predicate<Element> elementFilter) {
+    Elements elements = doc.select(selector);
+    return elements.stream().filter(elementFilter).map(elementMapper).collect(Collectors.toList());
+  }
+
+  public Document getDocument(final String pageUrl) throws IOException {
+    return new Document(pageUrl);
   }
 }
