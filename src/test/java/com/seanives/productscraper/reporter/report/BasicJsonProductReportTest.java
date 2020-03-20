0f8a6bab6ccd532869bf95f0761655e05f759c41
Sean Ives <me@seanives.com>
2020-03-20 15:58:58 +0000

    test(reporter): tests for BasicJsonProductReport (full implementation)

--- src/test/java/com/seanives/productscraper/reporter/report/BasicJsonProductReportTest.java
+++ src/test/java/com/seanives/productscraper/reporter/report/BasicJsonProductReportTest.java
@@ -1,15 +1,20 @@
 package com.seanives.productscraper.reporter.report;
 
+import com.seanives.productscraper.errors.parser.UnableToParseProductDetailsException;
 import com.seanives.productscraper.model.ProductResultsModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Collections;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class BasicJsonProductReportTest {
 
@@ -20,20 +25,41 @@ public class BasicJsonProductReportTest {
 
   public BasicJsonProductReportTest() {
     MockitoAnnotations.initMocks(this);
+  }
+
+  @BeforeEach
+  void beforeEach() {
     report = new BasicJsonProductReport(productResultsModel, reportRenderer);
   }
 
   @Test
   @DisplayName("should render a valid report")
   void render() {
+    doReturn(Collections.emptyMap()).when(reportRenderer).getRendered(any());
     report.render();
     verify(reportRenderer, times(1)).getRendered(productResultsModel);
   }
 
-  @Test
-  @DisplayName("should convert the rendered report to a string")
-  void convertToString() {
-    String result = report.toString();
-    assertThat(result, is(notNullValue()));
+  @Nested
+  @DisplayName("toString")
+  public class ConvertToStringTests {
+
+    @Test
+    @DisplayName("should convert the rendered report to a string")
+    void convertToString() {
+      doReturn(Collections.emptyMap()).when(reportRenderer).getRendered(any());
+      doReturn("xxx").when(reportRenderer).getJsonString(any());
+      report.render();
+      String result = report.toString();
+      assertThat(result, is(equalTo("xxx")));
+    }
+
+    @Test
+    @DisplayName("should throw an exception the report has not been rendered")
+    void convertToStringThrows() {
+      assertThat(
+          assertThrows(IllegalStateException.class, () -> report.toString()).getMessage(),
+          is(equalTo("report has not been rendered")));
+    }
   }
 }
