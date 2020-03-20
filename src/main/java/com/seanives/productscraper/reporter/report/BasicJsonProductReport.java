f570947a20b2069d589c1e2a03302f1bd1097ca4
Sean Ives <me@seanives.com>
2020-03-20 15:59:51 +0000

    refactor(reporter): tests for BasicJsonProductReport (full implementation) passing

--- src/main/java/com/seanives/productscraper/reporter/report/BasicJsonProductReport.java
+++ src/main/java/com/seanives/productscraper/reporter/report/BasicJsonProductReport.java
@@ -2,24 +2,29 @@ package com.seanives.productscraper.reporter.report;
 
 import com.seanives.productscraper.model.ProductResultsModel;
 
+import java.util.Map;
+import java.util.Optional;
+
 public class BasicJsonProductReport implements Report {
 
   private final ProductResultsModel productResultsModel;
-  private final JsonReportRenderer jsonReportRenderer;
+  private final JsonReportRenderer<ProductResultsModel> jsonReportRenderer;
+
+  private Optional<Map<String,Object>> renderedReport = Optional.empty();
 
   public BasicJsonProductReport(
-      final ProductResultsModel productResultsModel, final JsonReportRenderer jsonReportRenderer) {
+          final ProductResultsModel productResultsModel, final JsonReportRenderer<ProductResultsModel> jsonReportRenderer) {
     this.productResultsModel = productResultsModel;
     this.jsonReportRenderer = jsonReportRenderer;
   }
 
   @Override
   public void render() {
-    jsonReportRenderer.getRendered(productResultsModel);
+    renderedReport = Optional.of(jsonReportRenderer.getRendered(productResultsModel));
   }
 
   @Override
   public String toString() {
-    return null;
+    return jsonReportRenderer.getJsonString(renderedReport.orElseThrow(() -> new IllegalStateException("report has not been rendered")));
   }
 }
