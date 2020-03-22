eb957f75d942098d4c628a2d54e68006b72b560e
Sean Ives <me@seanives.com>
2020-03-22 13:06:05 +0000

    test(aggregator): test for full implementation of ProductResultsAggregator

--- src/main/java/com/seanives/productscraper/aggregator/aggregation/Aggregation.java
+++ src/main/java/com/seanives/productscraper/aggregator/aggregation/Aggregation.java
@@ -6,11 +6,11 @@ public class Aggregation<T extends Aggregatable> {
 
   static final double VAT_RATE = 0.2d; // 20%
 
-  double calculateGross(List<T> itemsToTotal) {
+  public double calculateGross(List<T> itemsToTotal) {
     return itemsToTotal.stream().map(Aggregatable::getUnitPrice).reduce(0.0d, Double::sum);
   }
 
-  double calculateVat(double gross) {
+  public double calculateVat(double gross) {
     return gross * VAT_RATE;
   }
 }
--- src/test/java/com/seanives/productscraper/aggregator/ProductResultsAggregatorTest.java
+++ src/test/java/com/seanives/productscraper/aggregator/ProductResultsAggregatorTest.java
@@ -6,26 +6,43 @@ import com.seanives.productscraper.model.ProductTotalModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class ProductResultsAggregatorTest {
 
   @Mock ProductModel product;
   @Mock Aggregation<ProductModel> productAggregation;
 
+  List<ProductModel> productList =  Arrays.asList(product);
+
   @Spy
   ProductResultsAggregator aggregator =
-      new ProductResultsAggregator(Arrays.asList(product), productAggregation);
+      new ProductResultsAggregator(productList, productAggregation);
+
+  public ProductResultsAggregatorTest() {
+    MockitoAnnotations.initMocks(this);
+  }
 
   @Test
   @DisplayName("should generate a valid product results total")
   void generateTotal() {
+    doReturn(20.20d).when(productAggregation).calculateGross(any());
+    doReturn(4.04d).when(productAggregation).calculateVat(anyDouble());
     ProductTotalModel total = aggregator.getTotal();
     assertThat(total, is(notNullValue()));
+    assertThat(total.getGross(), is(equalTo(20.20d)));
+    assertThat(total.getVat(), is(equalTo(4.04d)));
+    verify(productAggregation, times(1)).calculateGross(productList);
+    verify(productAggregation, times(1)).calculateVat(20.20d);
   }
 }
