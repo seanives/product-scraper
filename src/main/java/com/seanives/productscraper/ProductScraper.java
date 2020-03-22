56c6eef5026b3a18c10081ae79cf21dc437075ad
Sean Ives <me@seanives.com>
2020-03-22 21:02:40 +0000

    refactor(product scraper): test for ProductScraper class passing

--- src/main/java/com/seanives/productscraper/ProductScraper.java
+++ src/main/java/com/seanives/productscraper/ProductScraper.java
@@ -1,10 +1,27 @@
 package com.seanives.productscraper;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.seanives.productscraper.aggregator.AggregatorModule;
+import com.seanives.productscraper.parser.ParserModule;
 import com.seanives.productscraper.parser.ProductParser;
+import com.seanives.productscraper.presenter.PresenterModule;
+import com.seanives.productscraper.reporter.ReporterModule;
 
 public class ProductScraper {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Injector injector =
+        Guice.createInjector(
+            new ParserModule(),
+            new AggregatorModule(),
+            new ReporterModule(),
+            new PresenterModule());
 
-  public ProductScraper(final ProductParser parser) {}
+    new ProductScraper(injector.getInstance(ProductParser.class));
+  }
+
+  public ProductScraper(final ProductParser parser) {
+    parser.parseProducts();
+  }
 }
