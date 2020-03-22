c47206e28e89bcec416f2759007e4b4ba67d06ea
Sean Ives <me@seanives.com>
2020-03-22 21:00:27 +0000

    refactor(product scraper): test for ProductScraper class passing

--- src/main/java/com/seanives/productscraper/ProductScraper.java
+++ src/main/java/com/seanives/productscraper/ProductScraper.java
@@ -1,7 +1,10 @@
 package com.seanives.productscraper;
 
+import com.seanives.productscraper.parser.ProductParser;
+
 public class ProductScraper {
 
-    public static void main(String[] args) {
-    }
+  public static void main(String[] args) {}
+
+  public ProductScraper(final ProductParser parser) {}
 }
