5f4467b6c183c8aa457082556f028cd9a32688ed
Sean Ives <me@seanives.com>
2020-03-23 15:56:10 +0000

    test(formatter): tests for full implementation of ProductReportFormatting

--- src/test/java/com/seanives/productscraper/reporter/formatting/ProductReportFormattingTest.java
+++ src/test/java/com/seanives/productscraper/reporter/formatting/ProductReportFormattingTest.java
@@ -1,24 +1,90 @@
 package com.seanives.productscraper.reporter.formatting;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 
 public class ProductReportFormattingTest {
 
-  ProductReportFormatting formatting = new ProductReportFormatting();
+  ProductReportFormatting formatting = spy(new ProductReportFormatting());
 
-  @Test
-  public void mapKey() {
-    String mappedKey = formatting.mapKey("key");
-    assertThat(mappedKey, is(notNullValue()));
+  @Nested
+  @DisplayName("mapKey")
+  public class MapKeyTests {
+
+    @Test
+    @DisplayName("Should map an unknown key correctly")
+    public void mapKeyUnknown() {
+      String mappedKey = formatting.mapKey("key");
+      assertThat(mappedKey, is(equalTo("key")));
+    }
+
+    @Test
+    @DisplayName("Should map a known camel case key to snake case")
+    public void mapKeyCamelCase() {
+      String mappedKey = formatting.mapKey("kcalPer100g");
+      assertThat(mappedKey, is(equalTo("kcal_per_100g")));
+    }
   }
 
-  @Test
-  public void mapValue() {
-    Object mappedValue = formatting.mapValue("value");
-    assertThat(mappedValue, is(notNullValue()));
+  @Nested
+  @DisplayName("mapValue")
+  public class MapValueTests {
+    final Map<String, Object> nestedMap = new HashMap<String, Object>();
+    final List nestedList = Arrays.asList("value");
+    final Map<String, Object> testUnformattedRenderingMap =
+        new HashMap<String, Object>() {
+          {
+            put("key", nestedMap);
+          }
+        };
+    final Map<String, Object> testUnformattedRenderingList =
+        new HashMap<String, Object>() {
+          {
+            put("key", nestedList);
+          }
+        };
+
+    final ReportFormatter<ProductReportFormatting> reportFormatter =
+        spy(new ReportFormatter<>(formatting));
+
+    @Test
+    public void mapValue() {
+      Object mappedValue = formatting.mapValue(testUnformattedRenderingMap);
+      assertThat(mappedValue, is(notNullValue()));
+    }
+
+    @Test
+    @DisplayName("Should map a nested map")
+    public void mapValueNestedMap() {
+      reportFormatter.getFormatted(testUnformattedRenderingMap);
+      verify(formatting, times(1)).mapValue(nestedMap);
+      verify(reportFormatter, times(1)).getFormatted(testUnformattedRenderingMap);
+    }
+
+    @Test
+    @DisplayName("Should map a nested list")
+    public void mapValueNestedList() {
+      reportFormatter.getFormatted(testUnformattedRenderingList);
+      verify(formatting, times(1)).mapValue(nestedList);
+      verify(reportFormatter, times(1)).getFormatted(testUnformattedRenderingList);
+    }
+
+    @Test
+    @DisplayName("Should map a double to 2 decimal places")
+    public void mapValueDouble() {
+      formatting.mapValue(12.345678d);
+      verify(formatting, times(1)).roundToDP(12.345678d, 2);
+    }
   }
 
   @Test
