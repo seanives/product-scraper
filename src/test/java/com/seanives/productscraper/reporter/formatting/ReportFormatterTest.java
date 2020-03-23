fec11edfcf8f2b5740f48aebc24f2582e6ddcde3
Sean Ives <me@seanives.com>
2020-03-23 15:58:20 +0000

    test(formatter): tests for full implementation of ReportFormatter

--- src/test/java/com/seanives/productscraper/reporter/formatting/ReportFormatterTest.java
+++ src/test/java/com/seanives/productscraper/reporter/formatting/ReportFormatterTest.java
@@ -6,6 +6,7 @@ import org.mockito.MockitoAnnotations;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -32,9 +33,14 @@ public class ReportFormatterTest {
 
   @Test
   public void getFormattedMap() {
+    doReturn("key").when(formatting).mapKey(anyString());
+    doReturn("value").when(formatting).mapValue(anyString());
     Map<String, Object> formattedRendering =
         reportFormatter.getFormatted(testUnformattedRenderingMap);
     assertThat(formattedRendering, is(notNullValue()));
+    assertThat(formattedRendering.size(), is(equalTo(1)));
+    verify(formatting, times(1)).mapKey("key");
+    verify(formatting, times(1)).mapValue("value");
   }
 
   @Test
@@ -42,5 +48,8 @@ public class ReportFormatterTest {
     List formattedRendering =
         reportFormatter.getFormatted(Arrays.asList("value"));
     assertThat(formattedRendering, is(notNullValue()));
+    assertThat(formattedRendering.size(), is(equalTo(1)));
+    verify(formatting, times(0)).mapKey("key");
+    verify(formatting, times(1)).mapValue("value");
   }
 }
